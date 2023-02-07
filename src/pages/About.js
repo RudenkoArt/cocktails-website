860bcb84d2f8ba9b9c4931de65c9888550a92bae
RudenkoArt <rudnkoart@gmail.com>
2023-02-07 14:57:38 +0200

    complete error & about pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus, aut
+        ullam voluptates rem error enim obcaecati ut quam quibusdam blanditiis
+        vero. Voluptatum ad, adipisci ipsa in, repellendus at labore dignissimos
+        culpa assumenda aliquam totam nam atque soluta. Mollitia, porro ad.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
